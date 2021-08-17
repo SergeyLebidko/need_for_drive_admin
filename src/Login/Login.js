359e820c093ed9d312b9fa6d9adcd55ee6bf7093
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-17 17:20:22 +0300

    v-07 Созданы стили для контейнеров формы входа

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {ReactComponent as Logo} from '../content/images/logo.svg';
 import './Login.scss';
 
 function Login(){
     return (
         <div className="login">
-            <div className="login__content">
-                Здесь будет компонент входа в систему
-            </div>
+            <header className="login__logo_block">
+                <Logo/>
+                <h1 className="login__logo_caption">Need for drive</h1>
+            </header>
+            <main className="login__form_block">
+                <h1 className="login__form_caption">Вход</h1>
+                Здесь будет компонент входа в систему. А также ссылка на форму регистрации.
+            </main>
         </div>
     )
 }
