cc38df3ef6560bbfd9fd31a72eafbadc31cf9a87
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-18 15:05:19 +0300

    v-07 Добавлен компонент для панели администратора и ссылка на неё

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Login from './Login/Login';
+import Admin from './Admin/Admin';
 
 function App() {
     // TODO Для тестирования верстки сразу же переключаем пользователя на страницу логина. В будущем - пересмотреть поведение
@@ -11,6 +12,7 @@ function App() {
                     <Redirect to="/login"/>
                 </Route>
                 <Route path="/login" component={Login}/>
+                <Route path="/admin" component={Admin}/>
             </Switch>
         </HashRouter>
     );
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import BrandStamp, {LARGE_STAMP} from '../common_components/BrandStamp/BrandStamp';
 import TextField, {TEXT, PASSWORD} from '../common_components/TextField/TextField';
 import './Login.scss';
 
-function Login() {
+function Login({history}) {
+    // TODO При реализации функциональности добавить код процедуры авторизации
+    const handleLoginButtonClick = () => history.push('/admin');
+
     return (
         <div className="login">
             <BrandStamp size={LARGE_STAMP}/>
@@ -14,11 +18,17 @@ function Login() {
                 <TextField caption="Пароль" fieldType={PASSWORD}/>
                 <div className="login__control_block">
                     <Link to="/">Запросить доступ</Link>
-                    <button className="button button_blue">Войти</button>
+                    <button className="button button_blue" onClick={handleLoginButtonClick}>
+                        Войти
+                    </button>
                 </div>
             </div>
         </div>
     )
+}
+
+Login.propTypes = {
+    history: PropTypes.object
 }
 
 export default Login;
