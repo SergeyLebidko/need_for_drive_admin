7a7a9530ef169b1a57bfc5c847d661542547dc13
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-19 16:34:23 +0300

    v-07 Завершен SearchField.js

--- src/AdminHeader/AdminHeader.js
+++ src/AdminHeader/AdminHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchField from '../SearchField/SearchField';
 import './AdminHeader.scss';
 
 function AdminHeader(){
     return (
         <header className="admin_header">
-            Заголовок страницы
+            <SearchField/>
         </header>
     );
 }
