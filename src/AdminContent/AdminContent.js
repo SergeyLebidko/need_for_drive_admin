f0480d40bceea1d7c70e64994499b726c14dce4c
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-19 18:28:41 +0300

    v-07 Временная правка содержимого AdminContent.js

--- src/AdminContent/AdminContent.js
+++ src/AdminContent/AdminContent.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './AdminContent.scss';
 
 function AdminContent() {
+    // TODO Удалить инлайновый стиль. Сейчас он нужен только для тестирования верстки страницы admin
     return (
-        <main className="admin_content">
-            Здесь будет контент (страницы работы со списками и объектами)
+        <main className="admin_content" style={{padding: '15px', color: 'gray'}}>
+            Здесь будет контент (компоненты для работы со списками и объектами)
         </main>
     )
 }
