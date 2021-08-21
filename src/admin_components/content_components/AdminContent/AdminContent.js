4c31ccd917281fdb0418eae7030a616ce018e843
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-21 14:08:36 +0300

    v-07 В AdminContent добавлен роутер компонентов

--- src/admin_components/content_components/AdminContent/AdminContent.js
+++ src/admin_components/content_components/AdminContent/AdminContent.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Switch, Route, useRouteMatch} from 'react-router-dom';
+import OrderList from '../OrderList/OrderList';
+import NoMatch from '../../../common_components/NoMatch/NoMatch';
 import './AdminContent.scss';
 
 function AdminContent() {
-    // TODO Удалить инлайновый стиль. Сейчас он нужен только для тестирования верстки страницы admin
+    const match = useRouteMatch();
+
     return (
-        <main className="admin_content" style={{padding: '15px', color: 'gray'}}>
-            Здесь будет контент (компоненты для работы со списками и объектами)
+        <main className="admin_content">
+            <Switch>
+                <Route path={`${match.path}/orders`} component={OrderList}/>
+                <Route path="*" component={NoMatch}/>
+            </Switch>
         </main>
     )
 }
---
+++ src/admin_components/content_components/OrderList/OrderList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OrderList.scss';
+
+function OrderList(){
+    return (
+        <div>
+            Здесь будет список заказов
+        </div>
+    );
+}
+
+export default OrderList;
