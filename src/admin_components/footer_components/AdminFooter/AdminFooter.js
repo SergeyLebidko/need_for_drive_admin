eca16370900ea31ea78d31e4386a86cfa980850e
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-21 11:45:53 +0300

    v-07 Правка ссылки в футере

--- src/admin_components/footer_components/AdminFooter/AdminFooter.js
+++ src/admin_components/footer_components/AdminFooter/AdminFooter.js
@@ -6,7 +6,7 @@ function AdminFooter(){
     return (
         <footer className="admin_footer">
             <Link to="/admin">Главная страница</Link>
-            <a href="https://hproger.ru/need_for_drive/">Сервис</a>
+            <a href="https://hproger.ru/need_for_drive/">Сервис NFD</a>
             <span>Copyright	&#169; 2021 Simbirsoft</span>
         </footer>
     );
