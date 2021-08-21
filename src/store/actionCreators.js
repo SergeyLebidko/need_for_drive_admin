bb4f4ab9ce1d78b47ed36bd5350f9b1d4cc9a153
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-21 13:03:05 +0300

    v-07 Создана функция getCorrectPage

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,4 +1,12 @@
 import * as act from './actions';
+import {fetchOrderList} from '../utils/fetch_utils';
+
+// Функция возвращает корректный номер страницы
+function getCorrectPage(page) {
+    let _page = (page === null || page === undefined) ? 0 : +page;
+    if (isNaN(_page)) _page = 0;
+    return _page
+}
 
 // Создатель действия для сохранения данных пунктов меню
 export function setMenuItems(menuItems) {
@@ -17,8 +25,10 @@ export function setFrame(frame) {
 }
 
 // Создатель действия для загрузки списка заказов
-export function loadOrderList(page){
-    return async (dispatch, getState) => {
-
+export function loadOrderList(page) {
+    return async dispatch => {
+        const _page = getCorrectPage(page);
+        const {count, data} = await fetchOrderList();
+        dispatch(setFrame({count, data, page: _page}));
     }
 }
--- src/utils/fetch_utils.js
+++ src/utils/fetch_utils.js
@@ -23,15 +23,10 @@ async function executeFetch(url, options = {}) {
 function createFullUrl(baseUrl, filters) {
     let url = `${baseUrl}/?limit=${LIMIT}`;
     const {page} = filters;
-
-    if (page === undefined || page === null) {
-        url += '&page=0';
-    } else {
-        url += `&page=${page}`
-    }
+    url += `&page=${page}`
     return url;
 }
 
-export async function loadOrderList(filters) {
+export async function fetchOrderList(filters = {}) {
     return await executeFetch(createFullUrl(ORDER_URL, filters));
 }
