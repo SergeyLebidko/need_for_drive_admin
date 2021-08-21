e0d1abeaa42c6f80352b4aa7860fe7f86b3a29ea
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-21 10:35:34 +0300

    v-07 Рефакторинг верстки панели администратора

---
+++ src/admin_components/footer_components/AdminFooter/AdminFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AdminFooter.scss';
+
+function AdminFooter(){
+    return (
+        <footer className="admin_footer">
+            Нижняя часть страницы
+        </footer>
+    );
+}
+
+export default AdminFooter;
