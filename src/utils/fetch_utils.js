f347a4f2943f6dd6941e8bc53b490ec8bfb58885
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-21 12:44:10 +0300

    v-07 Созданы базовые функции для получения данных

--- src/utils/fetch_utils.js
+++ src/utils/fetch_utils.js
@@ -1,7 +1,14 @@
-async function executeFetch(url, options) {
+import {LIMIT} from '../settings';
+import {DEFAULT_REQUEST_HEADERS, ORDER_URL} from '../urls';
+
+async function executeFetch(url, options = {}) {
+    let {headers} = options;
+    headers = headers ? {...headers, ...DEFAULT_REQUEST_HEADERS} : DEFAULT_REQUEST_HEADERS;
+    const _options = {...options, headers};
+
     let response;
     try {
-        response = await fetch(url, options);
+        response = await fetch(url, _options);
     } catch (err) {
         return Promise.reject({httpStatus: '', httpText: err.message})
     }
@@ -11,4 +18,20 @@ async function executeFetch(url, options) {
         return Promise.reject({httpStatus: response.status, httpText: text});
     }
     return await response.json();
+}
+
+function createFullUrl(baseUrl, filters) {
+    let url = `${baseUrl}/?limit=${LIMIT}`;
+    const {page} = filters;
+
+    if (page === undefined || page === null) {
+        url += '&page=0';
+    } else {
+        url += `&page=${page}`
+    }
+    return url;
+}
+
+export async function loadOrderList(filters) {
+    return await executeFetch(createFullUrl(ORDER_URL, filters));
 }
