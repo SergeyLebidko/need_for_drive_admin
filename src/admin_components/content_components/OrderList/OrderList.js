44f9dbde190340058eb2eef68f3ffc5963762639
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-21 16:38:28 +0300

    v-07 Создан простейший прототип кода вывода заказов

--- src/admin_components/content_components/OrderList/OrderList.js
+++ src/admin_components/content_components/OrderList/OrderList.js
@@ -1,14 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import Preloader from '../../../common_components/Preloader/Preloader';
+import ErrorPane from '../../../common_components/ErrorPane/ErrorPane';
 import {loadOrderList} from '../../../store/actionCreators';
+import {getFrame} from '../../../store/selectors';
 import './OrderList.scss';
-import ErrorPane from "../../../common_components/ErrorPane/ErrorPane";
 
 function OrderList() {
     let [done, setDone] = useState(false);
     let [error, setError] = useState(null);
+
+    const {data: items} = useSelector(getFrame);
 
     const location = useLocation();
     const dispatch = useDispatch();
@@ -30,7 +34,20 @@ function OrderList() {
                 (error ?
                         <ErrorPane error={error}/>
                         :
-                        'Здесь будет список заказов'
+                        <>
+                            <h1>Заказы</h1>
+                            <div>
+                                <div>
+                                    Здесь будут фильтры
+                                </div>
+                                <ul>
+                                    {items.map(item => <li key={item.id}>{item.id}</li>)}
+                                </ul>
+                                <div>
+                                    Здесь будет пагинация
+                                </div>
+                            </div>
+                        </>
                 )
                 :
                 <Preloader/>
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,1 +1,3 @@
 export const getMenuItems = state => state.menuItems;
+
+export const getFrame = state => state.frame;
