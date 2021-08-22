ec278265a57a31a7c1a6cd00109c2fd2a280ac0c
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-22 13:09:15 +0300

    v-07 Создан OrderCardPhoto

--- src/admin_components/content_components/OrderCard/OrderCard.js
+++ src/admin_components/content_components/OrderCard/OrderCard.js
@@ -1,31 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {DOMEN} from '../../../urls';
+import OrderCardPhoto from '../OrderCardPhoto/OrderCardPhoto';
 import './OrderCard.scss';
 
 function OrderCard({order}) {
-    let [hasPhotoError, setHasPhotoError] = useState(false);
-
-    // Извлекаем сведения об автомобиле. При этом учитываем, что они могут отсутствовать
-    const {carId} = order;
-    let path;
-    if (carId) {
-        path = carId.thumbnail.path;
-        path = path[0] === '/' ? `${DOMEN}${path}` : path;
-    }
 
     return (
         <li className="order_card">
-            {hasPhotoError ?
-                <div className="order_card__no_photo">Не удалось загрузить фото...</div>
-                :
-                (path ?
-                        <img src={path} className="order_card__photo" onError={() => setHasPhotoError(true)}/>
-                        :
-                        <div className="order_card__no_photo">Нет фото...</div>
-                )
-            }
-            {order.id}
+            <OrderCardPhoto order={order}/>
         </li>
     );
 }
