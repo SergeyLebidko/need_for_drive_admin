e30ea2d16c2600af0b7a063eaa6a8f08399732d8
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-23 11:37:30 +0300

    v-07 Теперь справочники загружаются только тогда, когда они нужны

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -36,22 +36,29 @@ export function setCatalog(catalogName, catalogData) {
 
 // Создатель действия для загрузки списка заказов
 export function loadOrderList(page) {
-    return async dispatch => {
+    return async (dispatch, getState) => {
         // Загружаем список заказов
         const _page = getCorrectPage(page);
         const orderList = await fetchOrderList(_page);
         dispatch(setFrame({count: orderList.count, data: orderList.data, page: _page}));
 
-        // Загружаем справочник со статусами заказов
-        const statusList = await fetchStatusList();
-        dispatch(setCatalog(STATUS_LIST_CATALOG, statusList.data));
 
-        // Загружаем список авто (как справочник моделей)
-        const carList = await fetchCarList();
-        dispatch(setCatalog(CAR_LIST_CATALOG, carList.data));
+        // Загружаем справочник со статусами заказов, если он еще не был загружен
+        if (!getState().catalog[STATUS_LIST_CATALOG]) {
+            const statusList = await fetchStatusList();
+            dispatch(setCatalog(STATUS_LIST_CATALOG, statusList.data));
+        }
 
-        // Загружаем список городов
-        const cityList = await fetchCityList();
-        dispatch(setCatalog(CITY_LIST_CATALOG, cityList.data));
+        // Загружаем список авто (как справочник моделей), если он еще не был загружен
+        if (!getState().catalog[CAR_LIST_CATALOG]) {
+            const carList = await fetchCarList();
+            dispatch(setCatalog(CAR_LIST_CATALOG, carList.data));
+        }
+
+        // Загружаем список городов, если он еще не был загружен
+        if (!getState().catalog[CITY_LIST_CATALOG]) {
+            const cityList = await fetchCityList();
+            dispatch(setCatalog(CITY_LIST_CATALOG, cityList.data));
+        }
     }
 }
