150ba3307ce697ad89d195071d5542a72ea92275
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-24 12:35:57 +0300

    v-07 В Selector.js возвращен defaultItem

--- src/common_components/Selector/Selector.js
+++ src/common_components/Selector/Selector.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Selector.scss';
 
-function Selector({items, handleSelect}) {
+function Selector({items, defaultItem, handleSelect}) {
     const handleChange = event => handleSelect(event.target.value);
 
+    // TODO Удалить тернарный оператор из defaultValue
     return (
-        <select onChange={handleChange} className="selector">
+        <select onChange={handleChange} defaultValue={defaultItem ? defaultItem.value : null} className="selector">
             {items.map(
                 item =>
                     <option key={item.value} value={item.value}>
@@ -19,6 +20,7 @@ function Selector({items, handleSelect}) {
 
 Selector.propTypes = {
     items: PropTypes.array,
+    defaultItem: PropTypes.object,
     handleSelect: PropTypes.func
 }
 
