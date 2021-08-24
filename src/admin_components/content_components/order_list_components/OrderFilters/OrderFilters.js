9e624e2aad451e872feefb6064fa1e85ec64b132
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-24 12:45:51 +0300

    v-07 Рефакторинг фильтра

--- src/admin_components/content_components/order_list_components/OrderFilters/OrderFilters.js
+++ src/admin_components/content_components/order_list_components/OrderFilters/OrderFilters.js
@@ -10,18 +10,21 @@ import {
     BEGIN_MONTH,
     BEGIN_WEEK,
     CAR_LIST_CATALOG,
-    CITY_LIST_CATALOG
+    CITY_LIST_CATALOG,
+    STATUS_LIST_CATALOG
 } from '../../../../settings';
 import {getCatalog} from '../../../../store/selectors';
 import './OrderFilters.scss';
 
 function OrderFilters() {
-    let [selectedDateValue, setSelectedDateValue] = useState(NO_FILTER_VALUE);
-    let [selectedCarValue, setSelectedCarValue] = useState(NO_FILTER_VALUE);
-    let [selectedCityValue, setSelectedCityValue] = useState(NO_FILTER_VALUE);
+    let [selectedDate, setSelectedDate] = useState(NO_FILTER_VALUE);
+    let [selectedCar, setSelectedCar] = useState(NO_FILTER_VALUE);
+    let [selectedCity, setSelectedCity] = useState(NO_FILTER_VALUE);
+    let [selectedStatus, setSelectedStatus] = useState(NO_FILTER_VALUE);
 
     const carList = useSelector(getCatalog(CAR_LIST_CATALOG));
     const cityList = useSelector(getCatalog(CITY_LIST_CATALOG));
+    const statusList = useSelector(getCatalog(STATUS_LIST_CATALOG));
 
     // Готовим данные для селектора времени
     const dateSelectorItems = [
@@ -34,30 +37,38 @@ function OrderFilters() {
         {name: 'За месяц', value: PER_MONTH},
     ];
 
-    const handleDateSelect = value => setSelectedDateValue(value);
+    const handleDateSelect = value => setSelectedDate(value);
 
     // Готовим данные для селектора моделей авто
     const carSelectorItems = [{name: 'Все модели', value: NO_FILTER_VALUE}];
     for (let {id, name} of carList)carSelectorItems.push({name, value: id});
 
-    const handleCarSelect = value => setSelectedCarValue(value);
+    const handleCarSelect = value => setSelectedCar(value);
 
     // Готовим данные для селектора городов
     const citySelectorItems = [{name: 'Любой город', value: NO_FILTER_VALUE}];
     for (let {id, name} of cityList) citySelectorItems.push({name, value: id});
 
-    const handleCitySelector = value => setSelectedCityValue(value);
+    const handleCitySelect = value => setSelectedCity(value);
+
+    // Готовим данные для селектора по статусам заказов
+    const statusSelectorItems = [{name: 'Любой статус', value: NO_FILTER_VALUE}];
+    for (let {id, name} of statusList) statusSelectorItems.push({name, value: id});
+
+    const handleStatusSelect = value => setSelectedStatus(value);
 
     // TODO Удалить тестовые выводы в консоль
-    console.log(selectedDateValue);
-    console.log(selectedCarValue);
-    console.log(selectedCityValue);
+    console.log(selectedDate);
+    console.log(selectedCar);
+    console.log(selectedCity);
+    console.log(selectedStatus);
 
     return (
         <div className="order_filters">
             <Selector items={dateSelectorItems} handleSelect={handleDateSelect}/>
             <Selector items={carSelectorItems} handleSelect={handleCarSelect}/>
-            <Selector items={citySelectorItems} handleSelect={handleCitySelector}/>
+            <Selector items={citySelectorItems} handleSelect={handleCitySelect}/>
+            <Selector items={statusSelectorItems} handleSelect={handleStatusSelect}/>
         </div>
     );
 }
