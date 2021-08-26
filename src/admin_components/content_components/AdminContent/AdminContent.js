ffce24f19bc5a23f814fc628a59c26b6b20b0835
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-26 14:27:30 +0300

    f-08 Удалены тестовые пункты меню

--- src/admin_components/content_components/AdminContent/AdminContent.js
+++ src/admin_components/content_components/AdminContent/AdminContent.js
@@ -2,12 +2,8 @@ import React from 'react';
 import {Switch, Route, useRouteMatch} from 'react-router-dom';
 import OrderList from '../order_list_components/OrderList/OrderList';
 import NoMatch from '../../../common_components/NoMatch/NoMatch';
-import ErrorPane from '../../../common_components/ErrorPane/ErrorPane';
 import {ORDER_LIST_APP_URL} from '../../../urls';
 import './AdminContent.scss';
-
-// TODO В дальнейшем удалить. Сейчас нужен только для демонстрации верстки страницы с ошибкой
-const ErrorSample = () => <ErrorPane error={{httpStatus: 500, httpText: 'Failed to fetch'}}/>;
 
 function AdminContent() {
     const match = useRouteMatch();
@@ -18,7 +14,6 @@ function AdminContent() {
             <Switch>
                 <Route exact path={match.path} component={null}/>
                 <Route path={`${match.path}/${ORDER_LIST_APP_URL}`} component={OrderList}/>
-                <Route path={`${match.path}/error`} component={ErrorSample}/>
                 <Route path="*" component={NoMatch}/>
             </Switch>
         </main>
--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {ReactComponent as CarListIcon} from './content/images/car_list_icon.svg';
 import {ReactComponent as OrderListIcon} from './content/images/order_list_icon.svg';
-import {ReactComponent as MenuItemIcon} from './content/images/menu_item_icon.svg';
 
 // Размер страницы данных
 export const LIMIT = 10;
@@ -29,56 +27,10 @@ export const CITY_FILTER_NAME = 'cityId';
 export const STATUS_FILTER_NAME = 'orderStatusId';
 
 // TODO При реализации функциональности дополнить список пунктами меню для переходов на уже реализованные компоненты
-// TODO Удалить данные для тестирования верстки
 export const MENU_ITEMS = [
     {
         title: 'Заказы',
         iconComponent: <OrderListIcon/>,
         href: 'orders'
-    },
-    {
-        title: 'Автомобили',
-        iconComponent: <CarListIcon/>,
-        href: 'cars'
-    },
-    {
-        title: 'Элемент 3',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element3'
-    },
-    {
-        title: 'Элемент 4',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element4'
-    },
-    {
-        title: 'Элемент 5',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element5'
-    },
-    {
-        title: 'Элемент 6',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element6'
-    },
-    {
-        title: 'Элемент 7',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element7'
-    },
-    {
-        title: 'Элемент 8',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element8'
-    },
-    {
-        title: 'Элемент 9',
-        iconComponent: <MenuItemIcon/>,
-        href: 'element9'
-    },
-    {
-        title: 'Пример ошибки',
-        iconComponent: <MenuItemIcon/>,
-        href: 'error'
     }
 ];
