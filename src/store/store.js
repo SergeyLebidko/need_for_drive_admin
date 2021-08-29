da7e83ddcd8d82805633c663a2484f65acf89d64
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-29 16:18:25 +0300

    f-08 Установлен redux-devtools-extension

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import * as reducers from './reducers'
 
 const combinedReducer = combineReducers(reducers);
-const store = createStore(combinedReducer, {}, applyMiddleware(thunk));
+const store = createStore(combinedReducer, {}, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
