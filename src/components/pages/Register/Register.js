1007490f6a4db9d0cf95553319606b31fb599156
SergeyLebidko <it.sergeyler@mail.ru>
2021-08-30 12:49:13 +0300

    f-08 Рефакторинг имен в Register.js

--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -13,18 +13,18 @@ import {ALL_CHARS, LETTER_CHARS, DIGIT_CHARS, PASSWORD_SIZE} from '../../../cons
 import './Register.scss';
 
 function Register() {
-    const [loginValue, setLoginValue] = useState('');
-    const [loginErrorText, setLoginErrorText] = useState(null);
+    const [login, setLogin] = useState('');
+    const [loginError, setLoginError] = useState(null);
 
-    const [passwordValue, setPasswordValue] = useState('');
-    const [passwordErrorText, setPasswordErrorText] = useState(null);
-    const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
-    const [confirmPasswordErrorText, setConfirmPasswordErrorText] = useState(null);
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(null);
+    const [confirm, setConfirm] = useState('');
+    const [confirmError, setConfirmError] = useState(null);
 
     const [hasShowPassword, setHasShowPassword] = useState(false);
 
     const [registerProcess, setRegisterProcess] = useState(false);
-    const [registerError, setRegisterError] = useState(null);
+    const [registerProcessError, setRegisterProcessError] = useState(null);
 
     const history = useHistory();
 
@@ -63,77 +63,77 @@ function Register() {
         return errors.join(' ');
     }
 
-    const getConfirmPasswordError = (valuePass, valueConfirm) => {
+    const getConfirmError = (valuePass, valueConfirm) => {
         if (valueConfirm !== valuePass) return 'Пароль и подтверждение не совпадают';
         return null;
     }
 
     const handleChangeLogin = event => {
         const nextValue = event.target.value;
-        setLoginErrorText(getLoginError(nextValue));
-        setLoginValue(nextValue);
+        setLoginError(getLoginError(nextValue));
+        setLogin(nextValue);
     }
 
     const handleChangePassword = event => {
         const nextValue = event.target.value;
-        setPasswordErrorText(getPasswordError(nextValue));
-        setPasswordValue(nextValue);
-        setConfirmPasswordErrorText(getConfirmPasswordError(nextValue, confirmPasswordValue));
+        setPasswordError(getPasswordError(nextValue));
+        setPassword(nextValue);
+        setConfirmError(getConfirmError(nextValue, confirm));
     }
 
-    const handleChangeConfirmPassword = event => {
+    const handleChangeConfirm = event => {
         const nextValue = event.target.value;
-        setConfirmPasswordErrorText(getConfirmPasswordError(passwordValue, nextValue));
-        setConfirmPasswordValue(nextValue);
+        setConfirmError(getConfirmError(password, nextValue));
+        setConfirm(nextValue);
     }
 
     const handleCreatePassword = () => {
         const createdPassword = getRandomString(PASSWORD_SIZE, true);
-        setPasswordValue(createdPassword);
-        setPasswordErrorText(null);
-        setConfirmPasswordValue(createdPassword);
-        setConfirmPasswordErrorText(null);
+        setPassword(createdPassword);
+        setPasswordError(null);
+        setConfirm(createdPassword);
+        setConfirmError(null);
         setHasShowPassword(true);
     };
 
-    const handleShowPasswordControlClick = () => setHasShowPassword(!hasShowPassword);
+    const handleShowPasswordClick = () => setHasShowPassword(!hasShowPassword);
 
     const handleRegisterButtonClick = event => {
         event.preventDefault();
 
-        const emptyError = loginValue.length === 0 || passwordValue.length === 0 || confirmPasswordValue.length === 0;
-        if (loginValue.length === 0) setLoginErrorText('Обязательное поле');
-        if (passwordValue.length === 0) setPasswordErrorText('Обязательное поле');
-        if (confirmPasswordValue.length === 0) setConfirmPasswordErrorText('Обязательное поле');
+        const emptyError = login.length === 0 || password.length === 0 || confirm.length === 0;
+        if (login.length === 0) setLoginError('Обязательное поле');
+        if (password.length === 0) setPasswordError('Обязательное поле');
+        if (confirm.length === 0) setConfirmError('Обязательное поле');
         if (emptyError) return;
 
-        if (loginErrorText || passwordErrorText || confirmPasswordErrorText) return;
+        if (loginError || passwordError || confirmError) return;
 
         // Отправляем данные для регистрации
         setRegisterProcess(true);
-        register(loginValue, passwordValue)
+        register(login, password)
             .then(() => {
                 history.push(`/${LOGIN_APP_URL}`);
             })
             .catch(err => {
-                setRegisterError(err);
+                setRegisterProcessError(err);
                 setRegisterProcess(false);
             });
     };
 
     const clearFormData = () => {
-        setLoginValue('');
-        setLoginErrorText(null);
-        setPasswordValue('');
-        setPasswordErrorText(null);
+        setLogin('');
+        setLoginError(null);
+        setPassword('');
+        setPasswordError(null);
         setHasShowPassword(false);
         setRegisterProcess(false);
-        setRegisterError(null);
+        setRegisterProcessError(null);
     }
 
     if (registerProcess) return <Preloader/>;
 
-    if (registerError) return <ErrorPane error={registerError} handleBackButtonClick={clearFormData}/>;
+    if (registerProcessError) return <ErrorPane error={registerProcessError} handleBackButtonClick={clearFormData}/>;
 
     return (
         <div className="register">
@@ -142,32 +142,32 @@ function Register() {
                 <h1 className="register__form_caption">Регистрация</h1>
                 <TextField
                     label="Логин"
-                    value={loginValue}
+                    value={login}
                     handleChangeValue={handleChangeLogin}
-                    errorText={loginErrorText}
+                    errorText={loginError}
                 />
                 <ControlledPasswordField
                     label="Пароль"
-                    value={passwordValue}
+                    value={password}
                     handleChangeValue={handleChangePassword}
-                    errorText={passwordErrorText}
+                    errorText={passwordError}
                     hasShow={hasShowPassword}
                 />
                 <div className="register__password_control_block">
                     <span className="register__create_password" onClick={handleCreatePassword}>
                         Создать пароль
                     </span>
-                    {!!passwordValue &&
-                    <span className="register__show_password_control" onClick={handleShowPasswordControlClick}>
+                    {!!password &&
+                    <span className="register__show_password_control" onClick={handleShowPasswordClick}>
                         {hasShowPassword ? 'Скрыть' : 'Показать'}
                     </span>
                     }
                 </div>
                 <ControlledPasswordField
                     label="Подтверждение пароля"
-                    value={confirmPasswordValue}
-                    handleChangeValue={handleChangeConfirmPassword}
-                    errorText={confirmPasswordErrorText}
+                    value={confirm}
+                    handleChangeValue={handleChangeConfirm}
+                    errorText={confirmError}
                     hasShow={hasShowPassword}
                 />
                 <div className="register__control_block">
