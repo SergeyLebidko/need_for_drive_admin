58e5a8f868b0db129a4c51beb2c37f64ff1fae0f
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-01 18:56:36 +0300

    v-9_f-10 Создан первый вариант верстки CarCardDescription.scss

--- src/components/admin/content/car_list/CarCardDescription/CarCardDescription.js
+++ src/components/admin/content/car_list/CarCardDescription/CarCardDescription.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {capitalize} from '../../../../../utils/common_utils';
 import './CarCardDescription.scss';
 
-function CarCardDescription({car}){
+function CarCardDescription({car}) {
     return (
-        <div>
-            Здесь будет описание модели {car.name}
+        <div className="car_card_description">
+            <span className="car_card_description__name">{car.name}</span>
+            {car.categoryId &&
+            <div>
+                <span className="car_card_description__cat_name">{car.categoryId.name}</span>
+                <span className="car_card_description__cat_description"> ({car.categoryId.description})</span>
+            </div>
+            }
+            <span className="car_card_description__description">{car.description}</span>
+            {(car.number && car.number !== 'undefined') &&
+            <span className="car_card_description__number">
+                {car.number}
+            </span>
+            }
+            <span className="car_card_description__price">Цена: от {car.priceMin} до {car.priceMax}&#8381;</span>
+            {car.tank &&
+            <div className="car_card_description__tank">
+                Топливо: {car.tank}%
+                {(car.tank >= 0 && car.tank <= 100) &&
+                <div className="car_card_description__tank_indicator">
+                    <div style={{width: `${car.tank}%`}}/>
+                </div>
+                }
+            </div>
+            }
+            {car.colors && car.colors.length > 0 &&
+            <div className="car_card_description__colors">
+                Доступные цвета:
+                <ul>
+                    {car.colors.map(color => <li key={color}>{capitalize(color)}</li>)}
+                </ul>
+            </div>
+            }
         </div>
     );
 }
