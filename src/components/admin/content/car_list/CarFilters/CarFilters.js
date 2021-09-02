dfa189b4c02e1008ae89134d43c55f3adc4e9224
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-02 14:48:05 +0300

    v-9_f-10 Добавлены поля для ввода цены и выбора уровня топлива

--- src/components/admin/content/car_list/CarFilters/CarFilters.js
+++ src/components/admin/content/car_list/CarFilters/CarFilters.js
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import {useSelector} from 'react-redux';
 import {getCatalog} from '../../../../../store/selectors';
 import Selector from '../../../../common/Selector/Selector';
+import TextField from '../../../../common/TextField/TextField';
 import {CAR_CATEGORY_CATALOG} from '../../../../../constants/settings';
 import {NO_FILTER_VALUE} from '../../../../../constants/settings';
 import './CarFilters.scss';
 
 function CarFilters() {
     const [selectedCategory, setSelectedCategory] = useState(NO_FILTER_VALUE);
+    const [selectedTank, setSelectedTank] = useState(NO_FILTER_VALUE);
 
     const categoryList = useSelector(getCatalog(CAR_CATEGORY_CATALOG));
 
@@ -15,13 +17,25 @@ function CarFilters() {
     const categorySelectorItems = [{value: NO_FILTER_VALUE, name: 'Все категории'}];
     for (const {id, name} of categoryList) categorySelectorItems.push({value: id, name});
 
-    // Обработчики выбора
+    // Готовим данные для селектора уровня топлива
+    const tankSelectorItems = [{value: NO_FILTER_VALUE, name: 'Любой уровень топлива'}];
+    for (let level = 10; level <= 100; level += 10) tankSelectorItems.push({
+        value: '' + level,
+        name: `Не менее ${level}%`
+    });
+
+    // Обработчики выбора/ввода значений
     const handleCategorySelect = value => setSelectedCategory(value);
+
+    const handleTankSelect = value => setSelectedTank(value);
 
     return (
         <div className="car_filters">
             <div className="car_filters__conditions">
                 <Selector items={categorySelectorItems} value={selectedCategory} handleSelect={handleCategorySelect}/>
+                <TextField placeholder="Цена от..."/>
+                <TextField placeholder="Цена до..."/>
+                <Selector items={tankSelectorItems} value={selectedTank} handleSelect={handleTankSelect}/>
             </div>
             <div className="car_filters__control">
                 <button className="button button_red">Сбросить</button>
--- src/components/common/TextField/TextField.js
+++ src/components/common/TextField/TextField.js
@@ -4,31 +4,38 @@ import classNames from 'classnames';
 import {getRandomString} from '../../../utils/common_utils';
 import './TextField.scss';
 
-function TextField({label, value, handleChangeValue, errorText}) {
+function TextField({label, value, handleChangeValue, errorText, placeholder}) {
     const inputClasses = classNames('text_input', {'text_input_error': !!errorText});
 
     const inputId = getRandomString();
 
     return (
         <div className="text_field">
-            <label htmlFor={inputId}>{label}</label>
+            {label && <label htmlFor={inputId}>{label}</label>}
             <input
                 type="text"
                 className={inputClasses}
                 value={value}
                 onChange={handleChangeValue}
                 id={inputId}
+                placeholder={placeholder ? placeholder : ''}
             />
             {errorText && <span className="text_field__error_text">{errorText}</span>}
         </div>
     )
 }
 
+TextField.defaultProps = {
+    label: null,
+    placeholder: null
+}
+
 TextField.propTypes = {
     label: PropTypes.string,
     value: PropTypes.string,
     handleChangeValue: PropTypes.func,
-    errorText: PropTypes.string
+    errorText: PropTypes.string,
+    placeholder: PropTypes.string
 }
 
 export default TextField;
