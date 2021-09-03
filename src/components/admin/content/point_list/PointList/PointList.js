e339de6af62bf0038855183e84c6babad7cbcf40
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-03 12:20:35 +0300

    v-9_f-10 Создан компонент PointList.js

---
+++ src/components/admin/content/point_list/PointList/PointList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PointList.scss';
+
+function PointList(){
+    return (
+        <div className="point_list">
+            <h1>Пункты выдачи</h1>
+        </div>
+    )
+}
+
+export default PointList;
