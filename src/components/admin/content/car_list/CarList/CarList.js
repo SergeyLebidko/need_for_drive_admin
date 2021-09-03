865074b831e28a03218186f5c63c4e6ac000dc86
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-03 20:18:18 +0300

    v-9_f-10 useListLoader внедрен в список автомобилей

--- src/components/admin/content/car_list/CarList/CarList.js
+++ src/components/admin/content/car_list/CarList/CarList.js
@@ -1,57 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import {useLocation} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import {useSelector} from 'react-redux';
+import React from 'react';
 import Paginator from '../../../../common/Paginator/Paginator';
 import ErrorPane from '../../../../common/ErrorPane/ErrorPane';
 import CarFilters from '../CarFilters/CarFilters';
 import CarCard from '../CarCard/CarCard';
-import {ADMIN_APP_URL, CAR_LIST_APP_URL} from '../../../../../constants/urls';
+import {CAR_LIST_APP_URL} from '../../../../../constants/urls';
 import {
     CATEGORY_FILTER_NAME,
-    PAGE_FILTER_NAME,
     PRICE_MAX_FILTER_NAME,
     PRICE_MIN_FILTER_NAME, TANK_FILTER_NAME
 } from '../../../../../constants/settings';
 import {loadCarList} from '../../../../../store/actionCreators';
-import {getFrame} from '../../../../../store/selectors';
-import {useGlobalPreloader} from '../../../../../store/hooks';
+import {useListLoader} from '../../../../../store/hooks';
 import './CarList.scss';
 
 function CarList() {
-    const [done, setDone] = useState(false);
-    const [error, setError] = useState(null);
-
-    const frame = useSelector(getFrame);
-    let items;
-    if (frame) items = frame.data;
-
-    const location = useLocation();
-    const dispatch = useDispatch();
-
-    const [showGlobalPreloader, hideGlobalPreloader] = useGlobalPreloader();
-
-    useEffect(() => {
-        // Предотвращаем выполнение ненужных действий, если компонент размонтирован
-        if (!location.pathname.startsWith(`/${ADMIN_APP_URL}/${CAR_LIST_APP_URL}`)) return;
-
-        const params = new URLSearchParams(location.search);
-        const page = params.get(PAGE_FILTER_NAME);
-        const categoryId = params.get(CATEGORY_FILTER_NAME);
-        const priceMin = params.get(PRICE_MIN_FILTER_NAME);
-        const priceMax = params.get(PRICE_MAX_FILTER_NAME);
-        const tank = params.get(TANK_FILTER_NAME);
-
-        showGlobalPreloader();
-        setError(null);
-        setDone(false);
-        dispatch(loadCarList(page, categoryId, priceMin, priceMax, tank))
-            .catch(err => setError(err))
-            .finally(() => {
-                hideGlobalPreloader();
-                setDone(true);
-            });
-    }, [location]);
+    const [done, error, items] = useListLoader(
+        CAR_LIST_APP_URL,
+        [CATEGORY_FILTER_NAME, PRICE_MIN_FILTER_NAME, PRICE_MAX_FILTER_NAME, TANK_FILTER_NAME],
+        loadCarList
+    );
 
     if (error) return <ErrorPane error={error}/>;
 
