5e93ce6a88e258705019265da8518e2b66094b4d
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-03 19:27:11 +0300

    v-9_f-10 Создана разметка для списка фильтров

--- src/components/admin/content/point_list/PointFilters/PointFilters.js
+++ src/components/admin/content/point_list/PointFilters/PointFilters.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './PointFilters.scss';
+import Selector from '../../../../common/Selector/Selector';
+import {useSelector} from 'react-redux';
+import {getCatalog} from '../../../../../store/selectors';
+import {CITY_LIST_CATALOG, NO_FILTER_VALUE} from '../../../../../constants/settings';
 
-function PointFilters(){
+function PointFilters() {
+    const [selectedCity, setSelectedCity] = useState(NO_FILTER_VALUE);
+
+    const cityList = useSelector(getCatalog(CITY_LIST_CATALOG));
+    const citySelectorItems = [{value: NO_FILTER_VALUE, name: 'Любой город'}];
+    for (const city of cityList) citySelectorItems.push({value: city.id, name: city.name});
+
+    const handleCitySelect = value => setSelectedCity(value);
+
     return (
         <div className="point_filters">
-            Список фильтров
+            <Selector items={citySelectorItems} value={selectedCity} handleSelect={handleCitySelect}/>
+            <button className="button button_red point_filters__reset_button">Сброс</button>
+            <button className="button button_blue point_filters__apply_button">Применить</button>
         </div>
     );
 }
