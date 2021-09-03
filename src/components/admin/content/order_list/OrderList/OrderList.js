aeeab1dcac1d80ff07cce92dc31c2fcac58a4461
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-03 20:14:29 +0300

    v-9_f-10 useListLoader внедрен в список заказов

--- src/components/admin/content/order_list/OrderList/OrderList.js
+++ src/components/admin/content/order_list/OrderList/OrderList.js
@@ -1,17 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import {useLocation} from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import {useSelector} from 'react-redux';
+import React from 'react';
 import ErrorPane from '../../../../common/ErrorPane/ErrorPane';
 import OrderFilters from '../OrderFilters/OrderFilters';
 import Paginator from '../../../../common/Paginator/Paginator';
 import OrderCard from '../OrderCard/OrderCard';
 import {loadOrderList} from '../../../../../store/actionCreators';
-import {getFrame} from '../../../../../store/selectors';
-import {useGlobalPreloader} from '../../../../../store/hooks';
-import {ADMIN_APP_URL, ORDER_LIST_APP_URL} from '../../../../../constants/urls';
+import {useListLoader} from '../../../../../store/hooks';
+import {ORDER_LIST_APP_URL} from '../../../../../constants/urls';
 import {
-    PAGE_FILTER_NAME,
     DATE_FROM_FILTER_NAME,
     CAR_FILTER_NAME,
     CITY_FILTER_NAME,
@@ -20,40 +15,11 @@ import {
 import './OrderList.scss';
 
 function OrderList() {
-    const [done, setDone] = useState(false);
-    const [error, setError] = useState(null);
-
-    const location = useLocation();
-    const dispatch = useDispatch();
-
-    const [showGlobalPreloader, hideGlobalPreloader] = useGlobalPreloader();
-
-    const frame = useSelector(getFrame);
-    let items;
-    if (frame) items = frame.data;
-
-    // При монтировании компонента - получаем с сервера список заказов
-    useEffect(() => {
-        // Предотвращаем выполнение ненужных действий, если компонент размонтирован
-        if (!location.pathname.startsWith(`/${ADMIN_APP_URL}/${ORDER_LIST_APP_URL}`)) return;
-
-        const params = new URLSearchParams(location.search);
-        const page = params.get(PAGE_FILTER_NAME);
-        const date = params.get(DATE_FROM_FILTER_NAME);
-        const car = params.get(CAR_FILTER_NAME);
-        const city = params.get(CITY_FILTER_NAME);
-        const status = params.get(STATUS_FILTER_NAME);
-
-        showGlobalPreloader();
-        setError(null);
-        setDone(false);
-        dispatch(loadOrderList(page, date, car, city, status))
-            .catch(err => setError(err))
-            .finally(() => {
-                hideGlobalPreloader();
-                setDone(true);
-            });
-    }, [location]);
+    const [done, error, items] = useListLoader(
+        ORDER_LIST_APP_URL,
+        [DATE_FROM_FILTER_NAME, CAR_FILTER_NAME, CITY_FILTER_NAME, STATUS_FILTER_NAME],
+        loadOrderList
+    );
 
     if (error) return <ErrorPane error={error}/>;
 
