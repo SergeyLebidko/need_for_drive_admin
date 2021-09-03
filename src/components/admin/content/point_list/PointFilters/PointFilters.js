80b7cc4229e0e1695c4ad00b814beab34be33e0f
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-03 19:37:14 +0300

    v-9_f-10 Создан код изменения URL при выборе города в фильтре

--- src/components/admin/content/point_list/PointFilters/PointFilters.js
+++ src/components/admin/content/point_list/PointFilters/PointFilters.js
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
-import './PointFilters.scss';
 import Selector from '../../../../common/Selector/Selector';
 import {useSelector} from 'react-redux';
 import {getCatalog} from '../../../../../store/selectors';
-import {CITY_LIST_CATALOG, NO_FILTER_VALUE} from '../../../../../constants/settings';
+import {useHistory} from 'react-router-dom';
+import {
+    CITY_FILTER_NAME,
+    CITY_LIST_CATALOG,
+    NO_FILTER_VALUE,
+    PAGE_FILTER_NAME
+} from '../../../../../constants/settings';
+import {ADMIN_APP_URL, POINT_LIST_APP_URL} from '../../../../../constants/urls';
+import './PointFilters.scss';
 
 function PointFilters() {
     const [selectedCity, setSelectedCity] = useState(NO_FILTER_VALUE);
+
+    const history = useHistory();
 
     const cityList = useSelector(getCatalog(CITY_LIST_CATALOG));
     const citySelectorItems = [{value: NO_FILTER_VALUE, name: 'Любой город'}];
@@ -14,11 +23,40 @@ function PointFilters() {
 
     const handleCitySelect = value => setSelectedCity(value);
 
+    const handleApplyClick = () => {
+        const params = new URLSearchParams(location.search);
+        params.set(PAGE_FILTER_NAME, '0');
+
+        if (selectedCity === NO_FILTER_VALUE) {
+            params.delete(CITY_FILTER_NAME);
+        } else {
+            params.set(CITY_FILTER_NAME, selectedCity);
+        }
+
+        console.log(`${ADMIN_APP_URL}/${POINT_LIST_APP_URL}?${params}`)
+        history.push(`/${ADMIN_APP_URL}/${POINT_LIST_APP_URL}?${params}`);
+    }
+
+    const handleResetClick = () => {
+        setSelectedCity(NO_FILTER_VALUE);
+        history.push(`/${ADMIN_APP_URL}/${POINT_LIST_APP_URL}?${PAGE_FILTER_NAME}=0`);
+    }
+
     return (
         <div className="point_filters">
             <Selector items={citySelectorItems} value={selectedCity} handleSelect={handleCitySelect}/>
-            <button className="button button_red point_filters__reset_button">Сброс</button>
-            <button className="button button_blue point_filters__apply_button">Применить</button>
+            <button
+                className="button button_red point_filters__reset_button"
+                onClick={handleResetClick}
+            >
+                Сброс
+            </button>
+            <button
+                className="button button_blue point_filters__apply_button"
+                onClick={handleApplyClick}
+            >
+                Применить
+            </button>
         </div>
     );
 }
