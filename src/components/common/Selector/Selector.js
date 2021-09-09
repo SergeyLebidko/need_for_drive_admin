9ec6e05fa74bd3415d998177bf832fdc38fded1d
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-09 11:03:58 +0300

    v-10_f-9 Правки стилей для Selector.js

--- src/components/common/Selector/Selector.js
+++ src/components/common/Selector/Selector.js
@@ -2,25 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Selector.scss';
 
-function Selector({items, value, handleSelect}) {
+function Selector({items, value, handleSelect, label, errorText}) {
     const handleChange = event => handleSelect(event.target.value);
 
     return (
-        <select onChange={handleChange} value={value} className="selector">
-            {items.map(
-                item =>
-                    <option key={item.value} value={item.value}>
-                        {item.name}
-                    </option>
-            )}
-        </select>
+        <div className="selector">
+            {label && <label>{label}</label>}
+            <select onChange={handleChange} value={value} className="select_input">
+                {items.map(
+                    item =>
+                        <option key={item.value} value={item.value} className="select_option">
+                            {item.name}
+                        </option>
+                )}
+            </select>
+            {errorText && <span>{errorText}</span>}
+        </div>
     );
 }
 
 Selector.propTypes = {
     items: PropTypes.array,
     value: PropTypes.string,
-    handleSelect: PropTypes.func
+    handleSelect: PropTypes.func,
+    label: PropTypes.string,
+    errorText: PropTypes.string
 }
 
 export default Selector;
