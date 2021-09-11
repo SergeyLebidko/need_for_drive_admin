d2ef50c9765c453a3403f628026fec7a5803a720
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-11 16:04:33 +0300

    v-10_f-9 Создан первый вариант верстки для Car.js

--- src/components/admin/content/car/Car/Car.js
+++ src/components/admin/content/car/Car/Car.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Car.scss';
 
-function Car(){
+function Car() {
     return (
-        <div>
-            Компонент для создания и редактирования автомобиля
+        <div className="car">
+            <h1 className="car__caption">Карточка автомобиля</h1>
+            <div className="car__content_wrapper">
+                <div className="car__content car__first_content_block">
+                    Фото
+                </div>
+                <div className="car__content car__second_content_block">
+                    Параметры
+                </div>
+            </div>
         </div>
     );
 }
