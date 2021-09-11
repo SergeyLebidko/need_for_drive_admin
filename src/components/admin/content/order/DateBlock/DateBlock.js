2290d953df1bc9c22ae33261b9d06fa14ad28fa2
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-11 10:52:16 +0300

    v-10_f-9 Завершена работа над селектором даты

--- src/components/admin/content/order/DateBlock/DateBlock.js
+++ src/components/admin/content/order/DateBlock/DateBlock.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import ru from 'date-fns/locale/ru';
 import DatePicker, {setDefaultLocale} from 'react-datepicker';
 import {getOrderDateFrom, getOrderDateTo} from '../../../../../store/selectors';
+import {setEntityField} from '../../../../../store/actionCreators';
 import 'react-datepicker/dist/react-datepicker.css';
 import './DateBlock.scss';
 
@@ -12,15 +13,28 @@ function DateBlock() {
     const dateFrom = useSelector(getOrderDateFrom);
     const dateTo = useSelector(getOrderDateTo);
 
+    const dispatch = useDispatch();
+
+    const commonDatePickerProps = {
+        showTimeInput: true,
+        dateFormat: 'Pp',
+        locale: ru,
+        timeCaption: 'Время',
+        isClearable: true
+    }
+
+    const handleChangeDateFrom = value => dispatch(setEntityField('dateFrom', +value));
+    const handleChangeDateTo = value => dispatch(setEntityField('dateTo', +value));
+
     return (
         <div className="date_block">
             <div className="date_block__from_block">
                 <label>Дата начала аренды</label>
-                <DatePicker selected={dateFrom}/>
+                <DatePicker selected={dateFrom} {...commonDatePickerProps} onChange={handleChangeDateFrom}/>
             </div>
             <div className="date_block__to_block">
                 <label>Дата окончания аренды</label>
-                <DatePicker selected={dateTo}/>
+                <DatePicker selected={dateTo} {...commonDatePickerProps} onChange={handleChangeDateTo}/>
             </div>
         </div>
     );
