ed9487ee1f4fa73e16b72adc8be6e58abb0ee2cd
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-11 13:34:48 +0300

    v-10_f-9 В CarBlock добавлено отображение фото автомобиля

--- src/components/admin/content/order/CarBlock/CarBlock.js
+++ src/components/admin/content/order/CarBlock/CarBlock.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import Selector from '../../../../common/Selector/Selector';
 import PhotoBlock from '../../../../common/PhotoBlock/PhotoBlock';
+import {getOrderCar} from '../../../../../store/selectors';
 import './CarBlock.scss';
 
+
 function CarBlock() {
+    const car = useSelector(getOrderCar);
+
     return (
         <div className="car_block">
             <div className="car_block__selectors">
                 <Selector label="Автомобиль"/>
                 <Selector label="Цвет"/>
             </div>
-            <PhotoBlock/>
+            <PhotoBlock photoPath={!!car && car.thumbnail.path}/>
         </div>
     );
 }
