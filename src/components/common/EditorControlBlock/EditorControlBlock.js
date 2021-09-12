ea15903f0f046d5bc06dc9dbb5ee311aa1186e05
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-12 10:48:49 +0300

    v-10_f-9 Revert "В EditorControlBlock добавлен код управления доступностью кнопок"

--- src/components/common/EditorControlBlock/EditorControlBlock.js
+++ src/components/common/EditorControlBlock/EditorControlBlock.js
@@ -1,47 +1,21 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import './EditorControlBlock.scss';
 
-function EditorControlBlock({handleSave, handleCancel, handleRemove, saveEnabled, cancelEnabled, removeEnabled}) {
-    const handleSaveButtonClick = () => {
-        if (saveEnabled && handleSave) handleSave();
-    }
-
-    const handleCancelButtonClick = () => {
-        if (cancelEnabled && handleCancel) handleCancel();
-    }
-
-    const handleRemoveButtonClick = () => {
-        if (removeEnabled && handleRemove) handleRemove();
-    }
-
-    const saveButtonClasses = classNames('button', 'button_blue', {'button_disabled': !saveEnabled});
-    const cancelButtonClasses = classNames('button', 'button_silver', {'button_disabled': !cancelEnabled});
-    const removeButtonClasses = classNames('button', 'button_red', {'button_disabled': !removeEnabled});
-
+function EditorControlBlock({handleSave, handleCancel, handleRemove}) {
     return (
         <div className="editor_control_block">
-            <button className={saveButtonClasses} onClick={handleSaveButtonClick}>Сохранить</button>
-            <button className={cancelButtonClasses} onClick={handleCancelButtonClick}>Отменить</button>
-            <button className={removeButtonClasses} onClick={handleRemoveButtonClick}>Удалить</button>
+            <button className="button button_blue" onClick={handleSave}>Сохранить</button>
+            <button className="button button_silver" onClick={handleCancel}>Отменить</button>
+            <button className="button button_red" onClick={handleRemove}>Удалить</button>
         </div>
     );
-}
-
-EditorControlBlock.defaultProps = {
-    saveEnabled: true,
-    cancelEnabled: true,
-    removeEnabled: true
 }
 
 EditorControlBlock.propTypes = {
     handleSave: PropTypes.func,
     handleCancel: PropTypes.func,
-    handleRemove: PropTypes.func,
-    saveEnabled: PropTypes.bool,
-    cancelEnabled: PropTypes.bool,
-    removeEnabled: PropTypes.bool
+    handleRemove: PropTypes.func
 }
 
 export default EditorControlBlock;
