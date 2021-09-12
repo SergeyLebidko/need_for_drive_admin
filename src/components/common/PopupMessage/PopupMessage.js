523eadc1667169a77bca7d81af7b630bfaa5dbe7
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-12 11:59:41 +0300

    v-10_f-9 Рефакторинг PopupMessage.js

--- src/components/common/PopupMessage/PopupMessage.js
+++ src/components/common/PopupMessage/PopupMessage.js
@@ -1,35 +1,39 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import classNames from 'classnames';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {ReactComponent as CancelIcon} from '../../../content/images/cancel_icon.svg';
 import {getPopupMessage} from '../../../store/selectors';
-import {setPopupMessage} from '../../../store/actionCreators';
 import {FAIL, SUCCESS} from '../../../constants/settings';
 import './PopupMessage.scss';
 
 function PopupMessage() {
-    const {status, text} = useSelector(getPopupMessage);
-    const dispatch = useDispatch();
+    const [hasVisible, setHasVisible] = useState(false);
+    const message = useSelector(getPopupMessage);
     const timer = useRef(null);
 
-    // Если сообщение изменяется пока компонент виден - снова запускаем таймер отображения сообщения
+    const {text, status} = message;
+
+    const hidePopupMessage = () => setHasVisible(false);
+
     useEffect(() => {
         clearTimeout(timer.current);
-        if (text) timer.current = setTimeout(hidePopupMessage, 3000);
-    }, [text]);
+        if (text) {
+            timer.current = setTimeout(hidePopupMessage, 3000);
+            setHasVisible(true);
+            return;
+        }
+        setHasVisible(false);
+    }, [message]);
 
     // Предотвращаем возможную попытку обновления состояния компонента по таймеру после его размонтирования
     useEffect(() => () => clearTimeout(timer.current), []);
 
-    const hidePopupMessage = () => dispatch(setPopupMessage(status, null));
-
     const handleCancelIconClick = () => hidePopupMessage();
 
-    // Сообщение, состоящее только из строки текста считаем сообщением об успехе некой операции
     const popupMessageClasses = classNames(
         'popup_message',
         {
-            'popup_message_visible': !!text,
+            'popup_message_visible': hasVisible,
             'fail_message': status === FAIL,
             'success_message': status === SUCCESS
         }
