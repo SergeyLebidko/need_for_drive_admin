35eb74547ae32e49d57ca197038876fcaf110165
SergeyLebidko <it.sergeyler@mail.ru>
2021-09-14 19:52:52 +0300

    v-10_f-9 Создан каркас для TextValueEditor.js

---
+++ src/components/common/TextValueEditor/TextValueEditor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TextValueEditor.scss';
+
+function TextValueEditor(){
+    return (
+        <div className="text_value_editor">
+
+        </div>
+    );
+}
+
+export default TextValueEditor;
